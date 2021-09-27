0c37c9d542026a075caf473370f5512dfbb4595d
DiegoCV <skdiegomxtr@gmail.com>
2021-09-27 18:47:10 -0500

    first commit

---
+++ ejer02/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var numero int
+var texto string
+var boleano bool
+
+func main() {
+	numero2, numero3, numero4, texto2, boleano := 1, 2, 3, "int", true
+	fmt.Println(numero2)
+	fmt.Println(numero3)
+	fmt.Println(numero4)
+	fmt.Println(texto2)
+	fmt.Println(boleano)
+}
